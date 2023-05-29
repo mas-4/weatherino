2d832157fd635bf8d8050ea21eed61780f461501
malan88 <m@stdwtr.io>
2023-05-29 09:59:25 -0400

    Add hello world tests

---
+++ server/tests/test_core.py
@@ -0,0 +1,16 @@
+from weatherino.core import add
+import pytest
+
+
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (2, 3, 5),
+    (3, 4, 7),
+    (4, 5, 9),
+    (5, 6, 11),
+    (6, 7, 13),
+])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
+
+
